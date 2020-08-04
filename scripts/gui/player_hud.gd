b4c00a244e2be70e90da4aa83dc6b3e3e270c85a
FrankAlbella <FrankAlbella@gmail.com>
2020-08-04 12:35:00 -0400

    Update player_hud more accurately represent player stats

--- scripts/gui/player_hud.gd
+++ scripts/gui/player_hud.gd
@@ -6,6 +6,9 @@ export(float) var _max_mana
 
 export(float) var animate_time = 0.1
 
+onready var life_bar = $life_bar_frame/life_bar
+onready var mana_bar = $mana_bar_frame/mana_bar
+
 func _ready():
 	if _portrait != null:
 		set_portrait(_portrait)
@@ -25,24 +28,24 @@ func get_portrait() -> StreamTexture:
 	return _portrait
 
 func set_health(health: float) -> void:
-	if $Tween != null and $life_bar != null:
-		$Tween.interpolate_property($life_bar, "value", $life_bar.value, health, animate_time, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	if $Tween != null and life_bar != null:
+		$Tween.interpolate_property(life_bar, "value", life_bar.value, health, animate_time, Tween.TRANS_LINEAR, Tween.EASE_IN)
 		$Tween.start()
 
 func set_mana(mana: float) -> void:
-	if $Tween != null and $mana_bar != null:
-		$Tween.interpolate_property($mana_bar, "value", $mana_bar.value, mana, animate_time, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	if $Tween != null and mana_bar != null:
+		$Tween.interpolate_property(mana_bar, "value", mana_bar.value, mana, animate_time, Tween.TRANS_LINEAR, Tween.EASE_IN)
 		$Tween.start()
 
 func set_max_health(new_max_health: float) -> void:
 	_max_health = new_max_health
-	if $life_bar != null:
-		$life_bar.max_value = _max_health
+	if life_bar != null:
+		life_bar.max_value = _max_health
 
 func set_max_mana(new_max_mana: float) -> void:
 	_max_mana = new_max_mana
-	if $mana_bar != null:
-		$mana_bar.max_value = _max_mana
+	if mana_bar != null:
+		mana_bar.max_value = _max_mana
 
 func get_max_health() -> float:
 	return _max_health
