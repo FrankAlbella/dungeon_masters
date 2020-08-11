c3416a138cff20fbdf0b40d598d6741dbe214c08
FrankAlbella <FrankAlbella@gmail.com>
2020-08-11 11:50:46 -0400

    Optimize magic_missile timekeeping

--- scripts/props/magic_missile.gd
+++ scripts/props/magic_missile.gd
@@ -7,6 +7,8 @@ export var damage = 1
 export var heal = 0
 export var mana_cost = 0.5
 export var friendly_fire = true
+export var despawn_time = 3
+var time_passed = 0
 
 var source 
 var velocity
@@ -15,9 +17,14 @@ func fire(vel) -> void:
 	velocity = vel
 
 func _physics_process(delta):
+	time_passed += delta
+	
 	var collision_data = move_and_collide(velocity * delta * speed)
 	if collision_data:
-		_on_magic_missile_body_entered(collision_data.collider)	
+		_on_magic_missile_body_entered(collision_data.collider)
+		
+	if time_passed >= despawn_time:
+		queue_free()
 
 func get_source():
 	return source
@@ -27,7 +34,4 @@ func _on_magic_missile_body_entered(body):
 		body.take_damage(damage, source)
 		body.heal(heal)
 	
-	queue_free()
-
-func _on_Timer_timeout():
 	queue_free()
