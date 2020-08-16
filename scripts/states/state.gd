2bf4055062ddea765a7c3c95dcde4479fb1b7bde
FrankAlbella <FrankAlbella@gmail.com>
2020-08-16 14:39:26 -0400

    Add underscores to parameters in state.gd to avoid warnings

--- scripts/states/state.gd
+++ scripts/states/state.gd
@@ -14,14 +14,14 @@ func exit():
 	Global.log_warning(name+" state: exit not implemented")
 	return
 
-func handle_input(event):
+func handle_input(_event):
 	Global.log_warning(name+" state: handle_input not implemented")
 	return
 
-func update(delta):
+func update(_delta):
 	Global.log_warning(name+" state: update not implemented")
 	return
 
-func _on_animation_finished(anim_name):
+func _on_animation_finished(_anim_name):
 	Global.log_warning(name+" state: _on_animation_finished not implemented")
 	return
